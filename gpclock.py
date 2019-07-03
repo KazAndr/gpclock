58d4fee2f0ef09a8b39b09a1befd092e1fe805ee
Andrey Kazantsev <kaz.prao@bk.ru>
2019-07-03 14:59:44 +0300

    The code for reading profile and consequence pulses was added and tested

--- gpclock.py
+++ gpclock.py
@@ -10,16 +10,27 @@ import sys
 import glob
 
 import numpy as np
+import matplotlib.pyplot as plt
+
 
 def read_header(filename):
     header = {}
     with open(filename, 'r') as file:
-        file.readline() # Skip first uninform line in file ### HEADER
+        file.readline()  # Skip first uninform line in file ### HEADER
         for i in range(11):
             key, value = file.readline().rstrip().split(': ')
             header[key] = value
     return header
 
+
 files = sorted(glob.glob('data4test/*'))
+files_imps = sorted(glob.glob('data4test/c*'))
 
 print(read_header(files[0]))
+prf = np.genfromtxt(files[0], skip_header=14).T
+prfimps = np.genfromtxt(files_imps[0], skip_header=14).T
+
+seque_imps = np.hstack(prfimps[1:])
+plt.close()
+plt.plot(seque_imps)
+plt.show()
