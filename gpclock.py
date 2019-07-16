c728dc3f6217a58c537e58798a3010f194d34c1d
Andrey Kazantsev <kaz.prao@bk.ru>
2019-07-16 14:11:15 +0300

    Function for calculation time delay b/t two array with any(within reasonable limits) time_start and size of array was added.

--- gpclock.py
+++ gpclock.py
@@ -25,28 +25,57 @@ def read_header(filename):
             header[key] = value
     return header
 
+def delay_resizer(array1, array2):
+    """docstring for delay_resizer"""
+    if len(array1) == len(array2):
+        cf = np.correlate(array1, array2, 'full')
+        delay = ((len(cf)-1)/2)-np.argmax(cf)
 
-def get_time_delay(array1, array2):
+    elif len(array1) > len(array2):
+        array2 = np.append(array2, np.zeros(len(array1) - len(array2)))
+        cf = np.correlate(array1, array2, 'full')
+        # delay = (l_frame, r_frame)
+        delay = ((len(cf)-1)/2)-np.argmax(cf)
+
+    elif len(array1) < len(array2):
+        array1 = np.append(array1, np.zeros(len(array2) - len(array1)))
+        cf = np.correlate(array2, array1, 'full')
+        delay = -(((len(cf)-1)/2)-np.argmax(cf))
+    return delay
+
+
+def get_time_delay(ts1, ts2, array1, array2):
+
     """
     Description: return corfunction as result
     """
-    corfunc = np.correlate(array1, array2, 'full')
-    return corfunc
+    if ts1 == ts2:
+        delay = delay_resizer(array1, array2)
+
+    elif ts1 > ts2:
+        array1 = np.append(np.zeros(ts1 - ts2), array1)
+        delay = delay_resizer(array1, array2)
+
+    elif ts1 < ts2:
+        array2 = np.append(np.zeros(ts2 - ts1), array2)
+        delay = delay_resizer(array1, array2)
+
+    return delay
 
 
-FILES = sorted(glob.glob('data4test/*'))
-FILES_IMP = sorted(glob.glob('data4test/c*'))
+FILES = sorted(glob.glob('data4test/AP/*'))
+FILES_IMP = sorted(glob.glob('data4test/PULSES/*'))
 
-print(read_header(FILES[0]))
+# print(read_header(FILES[0]))
 prf = np.genfromtxt(FILES[0], skip_header=14).T
 prfimps = np.genfromtxt(FILES_IMP[0], skip_header=14).T
 
 seque_imps = np.hstack(prfimps[1:])
 
-plt.close()
-plt.plot(seque_imps)
-plt.axhline(3*np.std(seque_imps) + np.median(seque_imps))
-plt.show()
+# plt.close()
+# plt.plot(seque_imps)
+# plt.axhline(3*np.std(seque_imps) + np.median(seque_imps))
+# plt.show()
 
 need_points = 10000
 start_noise = np.random.normal(
@@ -61,16 +90,21 @@ end_noise = np.random.normal(
 
 seque_imps_pl_noise = np.hstack([start_noise, seque_imps, end_noise])
 
-plt.close()
-plt.plot(seque_imps_pl_noise)
-# plt.axhline(3*np.std(seque_imps) + np.median(seque_imps))
-plt.show()
 
-cf = get_time_delay(seque_imps_pl_noise, np.roll(seque_imps_pl_noise, 10))
-
-plt.close()
-plt.title(np.argmax(cf))
-plt.plot(cf)
-# plt.axhline(3*np.std(seque_imps) + np.median(seque_imps))
-plt.show()
-print(len(seque_imps_pl_noise))
+# plt.close()
+# plt.title(np.argmax(cf))
+# plt.plot(cf)
+# # plt.axhline(3*np.std(seque_imps) + np.median(seque_imps))
+# plt.show()
+ts1, ts2 = 100, 10
+for i in range(-2, 2):
+    moon_pulse = seque_imps_pl_noise[ts1:-800]
+    swich_moon_pulse = np.roll(seque_imps_pl_noise, i)[ts2:-122]
+    # cf = get_time_delay(swich_moon_pulse, moon_pulse)
+    cf = get_time_delay(ts1, ts2, moon_pulse, swich_moon_pulse)
+    print(i, cf)
+# plt.close()
+# plt.plot(moon_pulse)
+# plt.plot(swich_moon_pulse)
+# plt.xlim(7500, 17500)
+# plt.show()
