8958eabc7058be9f5cc8fc01012d308e83097108
Andrey Kazantsev <kaz.prao@bk.ru>
2019-07-18 09:29:59 +0300

    All work code was wrapped by if __name__ == '__main__'

--- gpclock.py
+++ gpclock.py
@@ -62,49 +62,49 @@ def get_time_delay(ts1, ts2, array1, array2):
 
     return delay
 
+if __name__ == "__main__":
+    FILES = sorted(glob.glob('data4test/AP/*'))
+    FILES_IMP = sorted(glob.glob('data4test/PULSES/*'))
 
-FILES = sorted(glob.glob('data4test/AP/*'))
-FILES_IMP = sorted(glob.glob('data4test/PULSES/*'))
+    # print(read_header(FILES[0]))
+    prf = np.genfromtxt(FILES[0], skip_header=14).T
+    prfimps = np.genfromtxt(FILES_IMP[0], skip_header=14).T
 
-# print(read_header(FILES[0]))
-prf = np.genfromtxt(FILES[0], skip_header=14).T
-prfimps = np.genfromtxt(FILES_IMP[0], skip_header=14).T
+    seque_imps = np.hstack(prfimps[1:])
 
-seque_imps = np.hstack(prfimps[1:])
+    # plt.close()
+    # plt.plot(seque_imps)
+    # plt.axhline(3*np.std(seque_imps) + np.median(seque_imps))
+    # plt.show()
 
-# plt.close()
-# plt.plot(seque_imps)
-# plt.axhline(3*np.std(seque_imps) + np.median(seque_imps))
-# plt.show()
+    need_points = 10000
+    start_noise = np.random.normal(
+        np.mean(seque_imps),
+        np.std(seque_imps),
+        need_points)
 
-need_points = 10000
-start_noise = np.random.normal(
-    np.mean(seque_imps),
-    np.std(seque_imps),
-    need_points)
+    end_noise = np.random.normal(
+        np.mean(seque_imps),
+        np.std(seque_imps),
+        need_points)
 
-end_noise = np.random.normal(
-    np.mean(seque_imps),
-    np.std(seque_imps),
-    need_points)
-
-seque_imps_pl_noise = np.hstack([start_noise, seque_imps, end_noise])
+    seque_imps_pl_noise = np.hstack([start_noise, seque_imps, end_noise])
 
 
-# plt.close()
-# plt.title(np.argmax(cf))
-# plt.plot(cf)
-# # plt.axhline(3*np.std(seque_imps) + np.median(seque_imps))
-# plt.show()
-ts1, ts2 = 100, 10
-for i in range(-2, 2):
-    moon_pulse = seque_imps_pl_noise[ts1:-800]
-    swich_moon_pulse = np.roll(seque_imps_pl_noise, i)[ts2:-122]
-    # cf = get_time_delay(swich_moon_pulse, moon_pulse)
-    cf = get_time_delay(ts1, ts2, moon_pulse, swich_moon_pulse)
-    print(i, cf)
-# plt.close()
-# plt.plot(moon_pulse)
-# plt.plot(swich_moon_pulse)
-# plt.xlim(7500, 17500)
-# plt.show()
+    # plt.close()
+    # plt.title(np.argmax(cf))
+    # plt.plot(cf)
+    # # plt.axhline(3*np.std(seque_imps) + np.median(seque_imps))
+    # plt.show()
+    ts1, ts2 = 100, 10
+    for i in range(-2, 2):
+        moon_pulse = seque_imps_pl_noise[ts1:-800]
+        swich_moon_pulse = np.roll(seque_imps_pl_noise, i)[ts2:-122]
+        # cf = get_time_delay(swich_moon_pulse, moon_pulse)
+        cf = get_time_delay(ts1, ts2, moon_pulse, swich_moon_pulse)
+        print(i, cf)
+    # plt.close()
+    # plt.plot(moon_pulse)
+    # plt.plot(swich_moon_pulse)
+    # plt.xlim(7500, 17500)
+    # plt.show()
